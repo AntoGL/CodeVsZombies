099e1a3761095a6b133deec29bbb87f0c1dc0d8c
Anto <goblin30003@gmail.com>
2022-05-05 16:57:57 +0500

    Добавил файл с константами и пару новых фильтров

---
+++ CodeVsZombies/GameConst.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Point.h"
+
+constexpr int WIDTH = 16000;
+constexpr int HEIGHT = 9000;
+
+constexpr Point LEFT_TOP_POINT = Point(0, 0);
+constexpr Point RIGHT_BOTTOM_POINT = Point(WIDTH, HEIGHT);
+
+constexpr double ASH_SPEED = 1000;
+constexpr double ASH_ATTACK_RANGE = 2000;
+
+constexpr double ZOMBIE_SPEED = 400;
+constexpr double ZOMBIE_ATTACK_RANGE = 400;
