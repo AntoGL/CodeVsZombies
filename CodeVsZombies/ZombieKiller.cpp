3f5b8e0e2ad65e6ecf6df67d05a1fd31cdc1fcb8
Anto <goblin30003@gmail.com>
2022-08-02 14:50:00 +0500

    Реализовал простой алгоритм спасения людей

--- CodeVsZombies/ZombieKiller.cpp
+++ CodeVsZombies/ZombieKiller.cpp
@@ -1,22 +1,87 @@
 #include "pch.h"
+#include <queue>
 #include "ZombieKiller.h"
+#include "GameReferee.h"
+
+std::vector<Point> ZombieKiller::GetMoves(const GameState& gameState)
+{
+	std::vector<Point> moves;
+	const auto& humans = gameState.GetHumans();
+	for (const auto& [_, human] : humans)
+		moves.push_back(human);
+
+	return moves;
+}
+
+std::vector<Turn> ZombieKiller::Calculate(const GameState& gameState)
+{
+	const auto moves = GetMoves(gameState);
+
+	std::vector<Turn> turns;
+	turns.reserve(moves.size());
+
+	static GameReferee referee;
+	for (const auto& p : moves)
+	{
+		auto copy = std::make_shared<GameState>(gameState);
+		turns.push_back(Turn{ p, copy });
+		referee.SetGameState(copy.get());
+		referee.Turn(p);
+	}
+
+	return turns;
+}
+
+std::vector<Turn> ZombieKiller::Calculate(const std::vector<Turn>& turns)
+{
+	std::vector<Turn> res;
+	for (const auto& turn : turns)
+	{
+		for (const auto& t : Calculate(turn))
+			res.push_back(t);
+	}
+
+	return res;
+}
+
+std::vector<Turn> ZombieKiller::Calculate(const Turn& turn)
+{
+	const auto& gameState = *turn.gameState;
+	const auto moves = GetMoves(gameState);
+
+	std::vector<Turn> turns;
+	turns.reserve(moves.size());
+
+	static GameReferee referee;
+	for (const auto& p : moves)
+	{
+		auto copy = std::make_shared<GameState>(gameState);
+		turns.emplace_back(Turn{ turn.fistMove, copy });
+		referee.SetGameState(copy.get());
+		referee.Turn(p);
+	}
+
+	return turns;
+}
 
 Point ZombieKiller::GetMovePoint(const GameState& gameState)
 {
-	const auto& humans = gameState.GetHumans();
-	const Unit& player = gameState.GetAsh();
-
-	double minD = 1000000;
-	Point p(0, 0);
-	for (const auto& human : humans)
+	auto turns = Calculate(gameState);
+	for (int i = 0; i < 10; i++)
 	{
-		const double d = player.DistanceTo(human.second);
-		if (d < minD)
+		turns = Calculate(turns);
+	}
+
+	Point res { 0, 0 };
+	int maxScore = -1;
+	for (const auto & turn : turns)
+	{
+		if (maxScore < turn.gameState->GetScore())
 		{
-			minD = d;
-			p = human.second;
+			res = turn.fistMove;
+			maxScore = turn.gameState->GetScore();
 		}
 	}
 
-	return p;
+	return res;
 }
--- CodeVsZombies/ZombieKiller.h
+++ CodeVsZombies/ZombieKiller.h
@@ -2,8 +2,19 @@
 #include "Point.h"
 #include "GameState.h"
 
+struct Turn
+{
+	Point fistMove;
+	std::shared_ptr<GameState> gameState;
+};
+
 class ZombieKiller
 {
+	std::vector<Point> GetMoves(const GameState& gameState);
+	std::vector<Turn> Calculate(const GameState& gameState);
+	std::vector<Turn> Calculate(const std::vector<Turn>& turns);
+	std::vector<Turn> Calculate(const Turn& turn);
+
 public:
 	Point GetMovePoint(const GameState& gameState);
 };
