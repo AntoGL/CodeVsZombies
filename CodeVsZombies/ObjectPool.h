662e575b013543d1e48c18496157b1efe9860bd8
Anto <goblin30003@gmail.com>
2022-09-16 07:20:59 +0500

    Добавил шаблонный get для конструирования объекта на месте существующего

--- CodeVsZombies/ObjectPool.h
+++ CodeVsZombies/ObjectPool.h
@@ -1,7 +1,5 @@
 #pragma once
 #include <list>
-#include <vector>
-#include "export.h"
 
 /*
  * Объектный пулл: https://habr.com/ru/post/205316/, https://ru.wikipedia.org/wiki/%D0%9E%D0%B1%D1%8A%D0%B5%D0%BA%D1%82%D0%BD%D1%8B%D0%B9_%D0%BF%D1%83%D0%BB
@@ -14,9 +12,10 @@
 template<class T, unsigned int chunk_size = 1024>
 class ObjectPool
 {
-	std::vector<T> buffer;
+	std::list<T> buffer;
 	std::list<T*> free_ptrs;
 public:
+	template<typename... Args>
 	inline T* get()
 	{
 		if (free_ptrs.empty())
@@ -27,22 +26,26 @@ public:
 		return res;
 	}
 
-	CVZ_EXPORT inline void free(T* obj);
+	template<typename... Args>
+	inline T* get(Args&&...args) {
+		T* obj = get();
+		new (obj) T(args...);
+		return obj;
+	}
+
+	inline void free(T* obj)
+	{
+		free_ptrs.push_back(obj);
+	}
 
 private:
 	void CreateObjectsAndFillFreePtrs();
 };
 
 template <class T, unsigned int chunk_size>
-void ObjectPool<T, chunk_size>::free(T* obj)
-{
-	free_ptrs.push_back(obj);
-}
-
-template <class T, unsigned int chunk_size>
 void ObjectPool<T, chunk_size>::CreateObjectsAndFillFreePtrs()
 {
-	for (int i = 0; i < chunk_size; ++i) {
+	for (unsigned int i = 0; i < chunk_size; ++i) {
 		buffer.emplace_back();
 		free_ptrs.emplace_back(&buffer.back());
 	}
--- CodeVsZombies_Tests/ObjectPool_Tests.cpp
+++ CodeVsZombies_Tests/ObjectPool_Tests.cpp
@@ -7,14 +7,33 @@ using namespace testing;
 struct TestObject
 {
 	static int CreatedObjectCount;
+	static int DeleatedObjectCount;
+
+	static int NextKey;
+	int key;
 
 	TestObject()
 	{
 		CreatedObjectCount++;
+
+		key = ++NextKey;
+	}
+
+	TestObject(const int key)
+		: key(key)
+	{
+		CreatedObjectCount++;
+	}
+
+	~TestObject()
+	{
+		DeleatedObjectCount++;
 	}
 };
 
 int TestObject::CreatedObjectCount = 0;
+int TestObject::DeleatedObjectCount = 0;
+int TestObject::NextKey = 0;
 
 TEST(ObjectPool_Test, get_FirstObject_ReturnNotNullObjectPointer)
 {
@@ -25,7 +44,7 @@ TEST(ObjectPool_Test, get_FirstObject_ReturnNotNullObjectPointer)
 	ASSERT_TRUE(nullptr != obj);
 }
 
-TEST(ObjectPool_Test, AfterFirstGet_NotCreateObject)
+TEST(ObjectPool_Test, Constructor_AfterCreate_NotCreateObject)
 {
 	TestObject::CreatedObjectCount = 0;
 	ObjectPool<TestObject, 1> pool;
@@ -41,4 +60,47 @@ TEST(ObjectPool_Test, get_AfterFirstGet_CreateChunkSizeObjects)
 	pool.get();
 
 	ASSERT_EQ(2, TestObject::CreatedObjectCount);
+}
+
+TEST(ObjectPool_Test, get_NoObjectsAvailable_CreateNewObjects)
+{
+	ObjectPool<TestObject, 2> pool;
+	pool.get();
+	pool.get();
+	TestObject::CreatedObjectCount = 0;
+
+	pool.get();
+
+	ASSERT_EQ(2, TestObject::CreatedObjectCount);
+}
+
+TEST(ObjectPool_Test, get_ObjectsAvailable_NoCreateNewObjects)
+{
+	ObjectPool<TestObject, 2> pool;
+	pool.get();
+	TestObject::CreatedObjectCount = 0;
+
+	pool.get();
+
+	ASSERT_EQ(0, TestObject::CreatedObjectCount);
+}
+
+TEST(ObjectPool_Test, free_AfterFree_NoDeleteObject)
+{
+	ObjectPool<TestObject, 2> pool;
+	TestObject* object = pool.get();
+	TestObject::DeleatedObjectCount = 0;
+
+	pool.free(object);
+
+	ASSERT_EQ(0, TestObject::DeleatedObjectCount);
+}
+
+TEST(ObjectPool_Test, get_WithArguments_ReturnObjectConstructedWithArgument)
+{
+	ObjectPool<TestObject, 2> pool;
+
+	const TestObject* object = pool.get(13);
+
+	ASSERT_EQ(13, object->key);
 }
